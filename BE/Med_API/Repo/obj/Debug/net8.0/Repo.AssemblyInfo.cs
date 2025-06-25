76c837943785b5b303c636ce02f191f8d92823fe
HSL-2003 <hoangsonlam97@gmail.com>
2025-06-25 11:31:45 +0700

    parent noti and confirm,approved student listed

--- BE/Med_API/Repo/obj/Debug/net8.0/Repo.AssemblyInfo.cs
+++ BE/Med_API/Repo/obj/Debug/net8.0/Repo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c9d493e16037c14f3c5c030df7e1ab348bf9a97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d9ee7c7073377fdf6a3144d6852186adfdbc0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
