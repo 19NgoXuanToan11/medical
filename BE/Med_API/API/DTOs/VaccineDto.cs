2957e9c2c6b88508c2c86aa31ea318d502cb448d
HSL-2003 <hoangsonlam97@gmail.com>
2025-07-19 19:10:56 +0700

    update crud vaccine

---
+++ BE/Med_API/API/DTOs/VaccineDto.cs
@@ -0,0 +1,70 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public static class VaccineDto
+{
+    public class ViewModel
+    {
+        public int VaccineId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Manufacturer { get; set; }
+        public string? BatchNumber { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public string? Dose { get; set; }
+        public string? AdministrationMethod { get; set; }
+        public string? Description { get; set; }
+        public bool? IsActive { get; set; }
+    }
+
+    public class Create
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(100)]
+        public string? Manufacturer { get; set; }
+
+        [StringLength(100)]
+        public string? BatchNumber { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+
+        [StringLength(50)]
+        public string? Dose { get; set; }
+
+        [StringLength(50)]
+        public string? AdministrationMethod { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public bool? IsActive { get; set; } = true;
+    }
+
+    public class Update
+    {
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [StringLength(100)]
+        public string? Manufacturer { get; set; }
+
+        [StringLength(100)]
+        public string? BatchNumber { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+
+        [StringLength(50)]
+        public string? Dose { get; set; }
+
+        [StringLength(50)]
+        public string? AdministrationMethod { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+} 
---
+++ BE/Med_API/Repo/IVaccineRepository.cs
@@ -0,0 +1,14 @@
+using DB;
+
+namespace Repo;
+
+public interface IVaccineRepository
+{
+    Task<IEnumerable<Vaccine>> GetAllVaccinesAsync();
+    Task<Vaccine?> GetVaccineByIdAsync(int id);
+    Task<Vaccine> CreateVaccineAsync(Vaccine vaccine);
+    Task UpdateVaccineAsync(Vaccine vaccine);
+    Task<bool> DeleteVaccineAsync(int id);
+    Task<Vaccine?> GetVaccineByNameAsync(string name);
+    Task<IEnumerable<Vaccine>> GetActiveVaccinesAsync();
+} 
